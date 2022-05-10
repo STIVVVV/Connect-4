327a476d8b625c5ed19a362bb5e0306608f8b8a0
STIVVVV <stevencanfield2000@gmail.com>
2022-05-10 11:49:02 -0400

    I can now place tiles.

--- index.js
+++ index.js
@@ -8,7 +8,29 @@ document.addEventListener('DOMContentLoaded', () => {
     {
        squares[i].onclick = () =>
        {
-           
+           //if the square below is taken, you can go ontop of it
+           if (squares[i + 7].classList.contains('taken'))
+           {
+               if (currentPlayer == 1)
+               {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-one')
+                    currentPlayer = 2
+                    displayCurrentPlayer.innerHTML = currentPlayer
+               }
+
+               else if(currentPlayer = 2)
+               {
+                    squares[i].classList.add('taken')
+                    squares[i].classList.add('player-two')
+                    currentPlayer = 1
+                    displayCurrentPlayer.innerHTML = currentPlayer
+               }
+           }
+           else
+            {
+                alert('Cant go here')
+            }
        }
         
     }
