ab2e24a0eb9070f54fc1d0ed654026c65fd85925
STIVVVV <stevencanfield2000@gmail.com>
2022-05-10 11:27:27 -0400

    Added grid and style

---
+++ index.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const result = document.querySelector('#result')
+    const displayCurrentPlayer = document.querySelector('#current-player') 
+    let currentPlayer = 1
+
+    for (let i = 0; i < squares.length; i++) 
+    {
+       squares[i].onclick = () =>
+       {
+           
+       }
+        
+    }
+
+})
